e51ed60127dd6e392c520193893542f6acc7e376
Daniel Santos <contatodanieljob@gmail.com>
2023-01-10 02:24:56 -0300

    feat: add enums

---
+++ src/main/java/com/example/models/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.example.models;
+
+public enum PaymentMethod {
+  cartao_credito,
+  cartao_debito,
+  pix,
+  boleto
+}
---
+++ src/main/java/com/example/models/PaymentStatus.java
@@ -0,0 +1,7 @@
+package com.example.models;
+
+public enum PaymentStatus {
+  PENDING,
+  PROCESSED_SUCCESS,
+  PROCESSED_ERROR
+}
